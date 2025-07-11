9cd47875cd341e3f03b4f06a42a2e4026e187313
Johann Bernhardt <johann.bernhardt@cariad.technology>
2025-07-11 10:19:32 +0200

    fix test coverage

--- myproject/main.py
+++ myproject/main.py
@@ -1,2 +1,6 @@
-if __name__ == "__main__":
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+if __name__ == "__main__":  # pragma: no cover - main
     print("Hello world!")
--- myproject/test_main.py
+++ myproject/test_main.py
@@ -1,2 +1,5 @@
-def test_main() -> None:
-    assert True
+from main import add
+
+
+def test_add() -> None:
+    assert add(3, 5) == 8
